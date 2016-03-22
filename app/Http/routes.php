e1a8b43be7321ea77ad7647d1717fc199d7d0033
IT7 <IT7@DESKTOP-VSUD6BE>
2016-03-22 05:26:52 -0700

    my first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/hello',function(){
+//    return 'Hello World!';
+//});
+
+Route::get('hello', 'Hello@index');
+
+Route::get('/hello/{name}', 'Hello@show');
+
+//------------------------------------------------------------------------
+
+Route::get('/','Front@index');
+Route::get('/products','Front@products');
+Route::get('/products/details/{id}','Front@product_details');
+Route::get('/products/categories','Front@product_categories');
+Route::get('/products/brands','Front@product_brands');
+Route::get('/blog','Front@blog');
+Route::get('/blog/post/{id}','Front@blog_post');
+Route::get('/contact-us','Front@contact_us');
+Route::get('/login','Front@login');
+Route::get('/logout','Front@logout');
+Route::get('/cart','Front@cart');
+Route::get('/checkout','Front@checkout');
+Route::get('/search/{query}','Front@search');
+
+//-------------------------------------------------------------------------
+
+Route::get('blade', function () {
+    $drinks = array('Vodka','Gin','Brandy');
+    return view('page',array('name' => 'The Raven','day' => 'Friday','drinks' => $drinks));
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
---
+++ resources/views/page.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+@parent
+
+<p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+<h2>{{$name}}</h2>    
+<p>This is my body content.</p>
+<h2>If Statement</h2>
+@if ($day == 'Friday')
+<p>Time to party</p>
+@else
+<p>Time to make money</p>
+@endif
+
+<h2>Foreach Loop</h2>
+@foreach ($drinks as $drink)
+{{$drink}} <br>
+@endforeach
+@endsection
