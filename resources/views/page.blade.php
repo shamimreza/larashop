7e5f7650ab558602bc4f28522de7b7f468023c57
IT7 <IT7@DESKTOP-VSUD6BE>
2016-03-24 23:28:05 +0600

    my second commit

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Page Title')
 
 @section('sidebar')
-@parent
+    @parent
 
-<p>This is appended to the master sidebar.</p>
+    <p>This is appended to the master sidebar.</p>
 @endsection
 
 @section('content')
@@ -13,13 +13,16 @@
 <p>This is my body content.</p>
 <h2>If Statement</h2>
 @if ($day == 'Friday')
-<p>Time to party</p>
+    <p>Time to party</p>
 @else
-<p>Time to make money</p>
+    <p>Time to make money</p>
 @endif
 
 <h2>Foreach Loop</h2>
 @foreach ($drinks as $drink)
 {{$drink}} <br>
 @endforeach
+
+<h2>Execute PHP Function</h2>
+<p>The date is {{date(' D M, Y')}}</p>
 @endsection
